36c34a00421e81ce4913d552f250365221bd9f32
OmHawaldar <91768070+OmHawaldar@users.noreply.github.com>
2024-12-10 20:18:14 +0530

    added optimal approach

--- Arrays/Rotate an Array by d - Counterclockwise or Left.java
+++ Arrays/Rotate an Array by d - Counterclockwise or Left.java
@@ -22,3 +22,43 @@ class Main {
     }
 }
 
+
+//Optimal Approach
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    
+    
+    
+     static void reverse(int[]arr,int start,int end){
+         while(start<end){
+              int temp=arr[start];
+               arr[start]=arr[end];
+               arr[end]=temp;
+               start++;
+               end--;
+               
+         }
+   
+        
+    }
+    
+    public static void main(String[] args) {
+        int []arr={45,78,79,88,12};
+        int n=arr.length;
+        
+        int d=10;
+        d%=n;
+        
+        reverse(arr,0,d-1);
+        reverse(arr,d,n-1);
+        reverse(arr,0,n-1);
+        
+        System.out.println(Arrays.toString(arr));
+ 
+}
+
+
+}
+
