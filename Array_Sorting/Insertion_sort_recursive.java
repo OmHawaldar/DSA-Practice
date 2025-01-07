0031ab8c7e42ec0b3ecfc44095315e25ef7f7891
OmHawaldar <91768070+OmHawaldar@users.noreply.github.com>
2025-01-07 23:52:05 +0530

    Create Insertion_sort_recursive.java

---
+++ Array_Sorting/Insertion_sort_recursive.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Main {
+    
+    
+    static void inser(int[]arr,int i,int n){
+        if(i==n){
+            return;
+        }
+        int j=i;
+        while(j>0&&arr[j-1]>arr[j]){
+            int temp=arr[j];
+            arr[j]=arr[j-1];
+            arr[j-1]=temp;
+            j--;
+        }
+        inser(arr,i+1,n);
+    }
+    public static void main(String[] args) {
+        
+        int arr[] = {13, 46, 24, 52, 20, 9};
+        int n = arr.length;
+        inser(arr,0,n);
+        System.out.print(Arrays.toString(arr));
+        
+    }
+}
+
