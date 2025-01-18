b0b4ecdcbd229f062756868d6c58a4f4c029b258
OmHawaldar <91768070+OmHawaldar@users.noreply.github.com>
2025-01-18 23:34:27 +0530

    Create Product_of_Arrays.java

---
+++ Blind_75/Product_of_Arrays.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4};
+        int n=arr.length;
+        int[] te = new int[n];
+        int i=0,j=0;
+        while(n>0){
+            int m=arr[i]
+            te[i]=m;
+            i++;
+            n--;
+        }
+       System.out.print(Arrays.toString(te));
+      
+        
+          }
+}
+
