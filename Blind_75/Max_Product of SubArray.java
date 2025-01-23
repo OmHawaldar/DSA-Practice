ebb7dc0d98d631ce4addecefa61ab51fc1922a00
OmHawaldar <91768070+OmHawaldar@users.noreply.github.com>
2025-01-23 23:53:02 +0530

    Update Max_Product of SubArray.java

--- Blind_75/Max_Product of SubArray.java
+++ Blind_75/Max_Product of SubArray.java
@@ -54,9 +54,9 @@ class Main {
         int n=arr.length;
         int p1=arr[0],p2=arr[0],res=arr[0];
         for(int i=1;i<n;i++){
-         int temp =Math.max(arr[i],Math.max(p1*arr[i],p2*arr[i]));
-         p2=Math.min(arr[i],Math.min(p1*arr[i],p2*arr[i]));
-        p1= temp;
+         int temp =Math.max(arr[i],Math.max(p1*arr[i],p2*arr[i]));   *********************************
+         p2=Math.min(arr[i],Math.min(p1*arr[i],p2*arr[i]));                OPTIMAL APPROACH
+        p1= temp;                                                    **********************************
             res=Math.max(res,p1);
             
         }
