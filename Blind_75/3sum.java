9193ffb2748ef0a8d7465c8ae551360fbc7d85cc
OmHawaldar <91768070+OmHawaldar@users.noreply.github.com>
2025-01-25 22:24:41 +0530

    Update 3sum.java

--- Blind_75/3sum.java
+++ Blind_75/3sum.java
@@ -4,6 +4,17 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(arr);
 
+          for (int i = 0; i < n; i++) {
+            //remove duplicates:
+            if (i != 0 && arr[i] == arr[i - 1]) continue;
+
+            //moving 2 pointers:
+            int j = i + 1;
+            int k = n - 1;
+         
+
+        return ans;
+
       
         
     }
