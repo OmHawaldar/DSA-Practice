e16066b5d3adc41a2c7f6ce6f79bac6f034682f1
OmHawaldar <91768070+OmHawaldar@users.noreply.github.com>
2025-01-25 22:24:13 +0530

    Create 3sum.java

---
+++ Blind_75/3sum.java
@@ -0,0 +1,10 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] arr) {
+        int n =arr.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(arr);
+
+      
+        
+    }
+}
